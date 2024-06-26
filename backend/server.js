ca150c3b4001496217d2862f93e212293b70adf1
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-06-26 19:01:52 +0530

    created some apis
    Tested some api using postman

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("API is running fastly");
 });
 
-app.listen(5000, console.log("Server is running on port 5000"));
+app.get("/api/chat", (req, res) => {
+  res.status(200).json(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const { id } = req.params;
+  const singleChat = chats.find((chat) => chat._id === id);
+
+  if (singleChat) {
+    res.status(200).json(singleChat);
+  } else {
+    res.status(404).json({ message: "Chat not found" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(`${PORT}`, console.log(`Server is running on port ${PORT}`));
