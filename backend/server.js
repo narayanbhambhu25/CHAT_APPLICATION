aa393238f8af4ca32c42f6d2b2b6b54f402ceceb
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-06-29 11:16:37 +0530

    Errorhandler
    errorhandling middleware
    
    Co-Authored-By: Rohitash001 <121787256+rohitash001@users.noreply.github.com>
    Co-Authored-By: Amankumar1811 <164324447+amankumar1811@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddlewares");
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound); // for errorhandling
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
