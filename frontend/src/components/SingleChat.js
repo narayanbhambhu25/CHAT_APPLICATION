95317a892ebcdd473648c2a5bfacfee7b99d14ff
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-07-29 17:33:57 +0530

    Dynamically update the MyChat Page
    Co-Authored-By: Rohitash001 <121787256+Rohitash001@users.noreply.github.com>
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,13 +1,48 @@
 import React from "react";
 import { ChatState } from "../Context/ChatProvider";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, IconButton, Text } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { getSender, getSenderFull } from "../config/Chatlogics";
+import ProfileModel from "./miscellaneous/ProfileModel";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
   return (
     <>
       {selectedChat ? (
-        <></>
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModel user={getSenderFull(user, selectedChat.users)} />
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                {/* {
+                   <UpdateGroupchatmodal
+                     fetchAgain ={fetchAgain}
+                     setFet chAgain = {setFetchAgain}
+                   /> } */}
+              </>
+            )}
+          </Text>
+        </>
       ) : (
         <Box
           display="flex"
