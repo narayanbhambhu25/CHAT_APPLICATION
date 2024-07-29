9e2afec9a2a5fa1d70f9d1b03286be94843860df
Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
2024-07-29 16:24:08 +0530

    Group chat created
    Co-Authored-By: Rohitash001 <121787256+Rohitash001@users.noreply.github.com>
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>

--- frontend/src/components/miscellaneous/GroupChatModal.js
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -77,7 +77,54 @@ const GroupChatModal = ({ children }) => {
     setSelectedUsers([...selectedUsers, userToAdd]);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (!groupChatName || !selectedUsers) {
+      toast({
+        title: "Please fill all the feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.post(
+        `/api/chat/group`,
+        {
+          name: groupChatName,
+          users: JSON.stringify(selectedUsers.map((u) => u._id)),
+        },
+        config
+      );
+
+      setChats([data, ...chats]);
+      onClose();
+      toast({
+        title: "New Group Chat Created!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    } catch (error) {
+      toast({
+        title: "Failed to Create the Chat!",
+        description: error.response.data,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
 
   return (
     <>
